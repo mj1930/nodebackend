95cd0e7be3518db632e036297c1da67c3dabc1ff
Mihir Jain <mihir.jain@smartdatainc.net>
2018-05-28 12:52:20 +0530

    added dist folder

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ var app = express();
 
 // view engine setup
 // Point static path to dist
-// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// // Catch all other routes and return the index file
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist/index.html'));
-// });
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.use(logger('dev'));
 app.use(express.json());
