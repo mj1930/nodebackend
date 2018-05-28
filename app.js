fa01aa624c46658f702893a6c9082832faafaafd
Mihir Jain <mihir.jain@smartdatainc.net>
2018-05-28 18:22:57 +0530

    added dist folder

--- app.js
+++ app.js
@@ -10,8 +10,16 @@ var bookingRoutes = require('./custom_modules/booking/routes/booking-routes');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch all other routes and return the index file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,9 +42,9 @@ var allowCrossDomain = function(req, res, next) {
 };
 app.use(allowCrossDomain);
 
-app.get('/', (req, res) => {
-  res.send('App working');
-})
+// app.get('/', (req, res) => {
+//   res.send('App working');
+// })
 app.use('/booking', bookingRoutes);
 
 // catch 404 and forward to error handler
