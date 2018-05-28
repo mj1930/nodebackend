221fcb06d68ebc72425d5dd7703bf2c60ebbfed4
Mihir Jain <mihir.jain@smartdatainc.net>
2018-05-28 16:41:10 +0530

    changed app.js

--- app.js
+++ app.js
@@ -10,13 +10,8 @@ var bookingRoutes = require('./custom_modules/booking/routes/booking-routes');
 var app = express();
 
 // view engine setup
-// Point static path to dist
-// app.use(express.static(path.join(__dirname, 'dist')));
-
-// // Catch all other routes and return the index file
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist/index.html'));
-// });
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
